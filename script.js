f30fd046f241b301ed09c854afd024970d8d6c45
afsona571 <afsonaa59@gmail.com>
2024-12-17 17:52:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let wrapper = document.querySelector(".wrapper")
+let name1 = prompt("Введите ваше имя ")
+let lastName = prompt("Ввидите ваше фамилю")
+let age = prompt("Ввидите ваше возрость ")
+let rang = prompt(`rang kirgizing`)
+
+
+
+let javob = document.querySelector(`.name`)
+let namei = javob.innerHTML = name1
+javob.style.color = rang
+
+let javobf = document.querySelector(`.familiya`)
+let familiya = javobf.innerHTML = lastName
+javobf.style.color = rang
+
+
+let javoby = document.querySelector(`.yosh`)
+let yosh = javoby.innerHTML = age
+javoby.style.color = rang
+
+let stylel = document.querySelector(`.FATHER`)
+// stylel.style.display = 'flex'
+// stylel.style.flex = 'column'
+// stylel.style.justify = 'center'
+
+
+
+
+
+
+
+
+// wrapper.innerHTML = `
+// <h2>ism:${name.toUpperCase()}</h2>
+// <h2>familiyangiz:${lastName.toUpperCase()}</h2>
+// <h2>yosh: ${age.toUpperCase()}</h2>
+
+
+
+
+// `
